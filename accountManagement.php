c2a3328c3c5b5b6b0658395ff2fc42efd0f444c4
Justin Werre <werrejustin@gmail.com>
2013-03-13 14:39:25 -0600

    Roll this back if it breaks

--- accountManagement.php
+++ accountManagement.php
@@ -11,11 +11,13 @@
     
     $con = new Connection();
     $con = $con->connect();
-        if (mysqlif (mysqli_errno($con)){
+    if (mysqli_errno($con)){
         
 		echo "Connection to Database Failed.";
         
-/:: Program Variable Declarations
+    }
+    
+    //:: Program Variable Declarations
     $userName  		    = $_SESSION['userName'];
     $userFname  		= $_SESSION['userFname'];
     $userLname  		= $_SESSION['userLname'];
--- manageNews.php
+++ manageNews.php
@@ -13,15 +13,20 @@
 	$con = new Connection();
     $con = $con->connect();
     if (mysqli_errno($con)){        
-		echo "Connection to Database Failed.";   }	//:: Program Variable Declarations
+		echo "Connection to Database Failed.";       
+    }
+	
+	//:: Program Variable Declarations
 	//$userAuth   		= $_SESSION['userAuth'];
 	$userName  		    =  "Tylor";//$_SESSION['userName'];
-	//$venueID  		    = $_SESSION['vId'];
+	//$venueID  		= $_SESSION['venueId'];
 	//$venueName  		= $_SESSION['venueName'];
 	
-	//:: Misc. c. Variabl$title = NULL;
+	$title = NULL;
 	$date  = NULL;
-	$comments = NULLVariab	s
+	$comments = NULL;
+			
+	//:: Misc. Variables
 	$error = " ";
 	$css = "";
 	$js = "";
@@ -29,116 +34,98 @@
 	createHead($css, $js);
 	createHeader($userName);
 	
-	echo '<div id="cont
-	//:: Draif(isset($_POST['submit'])){
-		$title = $		= $_POST['title'];
+	if(isset($_POST['submit'])){
+		$title 		= $_POST['title'];
 		$date  		= $_POST['newsDate'];
 		$comments 	= $_POST['comments'];
 		$regID 		= $_POST['regID'];
 		$newsID 	= $_POST['newsID'];
 			
-			len($title) <= 0 || strlen($date) <= 0 || strlen($comments) <=0 ){
+			
+		if(strlen($title) <= 0 || strlen($date) <= 0 || strlen($comments) <=0 ){
 			$error = "Error: All Fields are Mandatory.";
 		}
 		else{
-			setNews($con, $title, $date, $comments, 1004);
-			$err$regID, 1004);
+			setNews($con, $title, $date, $comments, $regID, 1004);
 			$error = "Update worked!";
 		}
 																			
 	}
-	 '<div id="content">';
+		
+	//:: Draws Content Blocks
+	echo '<div id="content">';
 	echo '<div class="headingDiv"><h2>Manage News</h2></div>';
 		 
-	echo '<div id="leftContent">';singleContent">';
+	echo '<div id="singleContent">';
 	echo '<div id="error">'.$error.'</div>';
-	$p = $_GET['action'];
+	$news = $_GET['action'];
 
-	swinews = $_GET['action'];
-
-	switch($newsecho '<div id="subhead">';
-			echo "<h3Create News Flash</span></h3>";
+	switch($news){
+		case "create":
+				
+			echo '<div id="subhead">';
+			echo "<h3><span class='yellow'>Create News Flash</span></h3>";
 			echo '</div>';
-			echo newNewsForm($con);
-
-		break;
-		
-		case "mo		
+			echo newNewsForm($con);		
 		
 		break;
 		
-		case "modify":
+		case "modify": 
 			if($_GET['id'] == NULL){
-				hea der('Location: manageNews.php?action=default');
-			}';
+				header('Location: manageNews.php?action=default');
+			}
+			echo '<div id="subhead">';
 			echo "<h3 class='center'><span class='yellow'>Modify News Flash</span></h3>";
 			echo '</div>';
 			echo modifyNewsForm($con);
+			
 		break;
 		
 		default:
-			echo '<di	
-		break;
-		
-		default:
-			echo '<div class="newNewsButtonate" >'.IMG("new.gif", "Create a new News Article.").'</a></div>';
+			echo '<div class="newNewsButton"><a href="manageNews.php?action=create" >'.IMG("new.gif", "Create a new News Article.").'</a></div>';
 				drawNewsTable($con);
 		break;
 		
 	}
-	   
-         
-        
-       
-	echo '</div>';
-	
-	echo '<div id="rightContent">';
-	//ec                        
+	                           
 	echo '</div>';	
 	echo "</div>"; //End of Content	
 	
 	createFoot();
 	
 	//:: End of Page
-	all data received through the form via POSTBACK and attempts to insert it into the database in a readable
-	 * manorthe Region select form element.
-	*/
-	function getRegionName($con){
-		static $data;
-		 
-		$sqlparam String $title The Title of the news article
+		
+//:: Start of Functions	
+	
+	/**
+	 * Take all data received through the form via POSTBACK and attempts to insert it into the database in a readable
+	 * manor.
+	 * @author Tylor Faoro
+	 *
+	 * @param Object $con The database connection object
+	 * @param String $title The Title of the news article
 	 * @param String $date A date/time string
 	 * @param String $comments The main content of the news flash
 	 * @param Int    $uid The id of the currently logged in user.
 	 *
 	 * @return none
 	*/
-	function setNews($con, $title, $date, $comments, $uid){
-		global $error;
-		$sql  = 'INSERT INTO news ';
-	regID, $uID){
+	function setNews($con, $title, $date, $comments, $regID, $uID){
 		global $error;		
-		EW_Date, NEW_Content, User_USE_ID)';
+		
+		$sql  = 'INSERT INTO news ';
+		$sql .= '(NEW_Title, NEW_Date, NEW_Content, User_USE_ID)';
 		$sql .= ' VALUES(';
 		$sql .= " '".$title."', ";
 		$sql .= " '".$date."', ";
 		$sql .= " '".$comments."', ";
-		$sql .= " '".$uid."' ";
+		$sql .= " '".$uID."' ";
 		$sql .= ')';
 		
-		
 		if(!mysqli_query($con, $sql)){
-ID."' ";
-		$sql .= ')';r($con));
-		}
-		else{
-			echo "Record should be in DB ";	
+			die('Error: '.mysqli_error($con));
 		}
 		
-		
-	}
-	
-	function mod
 		$newsID = getCurrentNewsID($con, $title);
 		setNewsRegionJunction($con, $newsID, $regID);
 	  
@@ -175,11 +162,11 @@ ID."' ";
 	 * When a user submits new news this function will fire to determine what the NEW_ID is of 
 	 * the news being submitted. The ID that is acquired is then used to create the Junction
 	 * between the News and Region Tables. This Function is written to work directly with
-	 * the setNewsRegionJunction() FunctiontRegionName($con){
-		static $data;
-		 
-		$sqlparam String $title The Title of the news article
-	 * @param String Acquired from parent function. Used to find the news just written.
+	 * the setNewsRegionJunction() Function.
+	 * @author Tylor Faoro
+	 *
+	 * @param Object $con The database connection object
+	 * @param String $title Acquired from parent function. Used to find the news just written.
 	 *
 	 * @return int $newsID Used by parent function to establish Junction between News and Region.
 	*/
@@ -204,31 +191,31 @@ ID."' ";
 	 * @return mixed $form A built form with necessary elements to create a news flash entry.
 	*/
 	function newNewsForm($con){
-        t" value="'.$default?action=modifys[1].'" name="title" />'."\n";
+        
+		$form  = '<div id="manageNews" >';
+        $form .= '<form method="POST" action="manageNews.php?action=create">'."\n";
+		$form .= '<label>Title:</label>';
+		$form .= '<input type="text" value="" name="title" />'."\n";
 		$form .= '<br /><br />';
-		
-		$form .= '<createDate:</label>';
-		$form .= '<input type="date" value="'.$defaults[2].'" name="newsDateame="newsDate" />';
+		$form .= '<label>Date:</label>';
+		$form .= '<input type="date" value="'.currTimeDate().'" name="newsDate" />';
 		$form .= '<br /><br />';
-		$form .= '<label on="regionName">Region:</label>';
-		$form .= '<select><span>'.getRegionName($con).'</span></select>';
+		$form .= '<label>Region:</label>';
+		$form .= '<select name="regID"><span>'.selectElementRegionOption($con).'</span></select>';
 		$form .= '<br /><br />';
-		$form .= '<label>Co>Region:</label>';
-		$form .= '<select name="regID"><span>'.selectElementRegionOptionxtarea>';
+		$form .= '<label>Comments: </label>';
+		$form .= '<textarea class="textarea" name="comments">';
+		$form .= '</textarea>';
 		$form .= '<br /><br />';
-		$form .= '<input name="commentst type="submit" class="button" value="Submit" />';
+		$form .= '<input type="submit" name="submit" class="button" value="Submit" />';
 		
 		$form .= '</form>';
-		$form .= '</div>nam .= '</div>';
+		$form .= '</div>';
 		
 		return $form;
 	}
 	
-	function modifyNewsForm($con){
-        $p = $_GET['id'];
-		
-		
-		$defaults = array(getNews($con, "NEW_ID", $p), getNews($con, "NEW_Title", $p), getNews($con, "NEW_Dat/**
+	/**
 	 * Draws the Modify News Form with all appropriate settings in place. The difference between
 	 * this form and newNewsForm() is all fields will be populated with default data via the Database.
 	 * @author Tylor Faoro
@@ -241,39 +228,37 @@ ID."' ";
         $p = $_GET['id'];
 				
 		$defaults = array( getNews($con, "NEW_Title", $p), getNews($con, "NEW_Date", $p), getNews($con, "REG_Name", $p), getNews($con, "NEW_Content", $p));
-		ext" value="'.$default?action=modifys[1].'" name="title" />'."\n";
+				
+		$form  = '<div id="manageNews" >';
+        $form .= '<form method="POST" action="manageNews.php?action=modify">'."\n";
+		$form .= '<input type="hidden" name="newsID" value='.$p.' />';
+		$form .= '<label>Title:</label>';
+		$form .= '<input type="text" value="'.$defaults[0].'" name="title" />'."\n";
 		$form .= '<br /><br />';
 		
 		$form .= '<label>Date:</label>';
-		$form input type="hidden" name="newsID" value='.$p.' />'abel>';
-		$form .= '<input type="date" value="'.$defaults[2].'" name="newsDate" />';
-		$fo0m .= '<br /><br />';
-		
-		$form .= '<label>Region:</label>';
-		$form .= '<select><span></span></select>';
+		$form .= '<input type="date" value="'.$defaults[1].'" name="newsDate" />';
 		$form .= '<br /><br />';
 		
-		$form .= 1<label>Comments: </label>';
+		$form .= '<label>Region:</label>';
+		$form .= '<select name="regionName" value=""><span>'.selectElementRegionOption($con).'</span></select>';
+		$form .= '<br /><br />';
+		
+		$form .= '<label>Comments: </label>';
 		$form .= '<textarea class="textarea">';
-		$form .= $defaults[4];
-		$form .= '</textare name="regionName" value=""><span>'.selectElementRegionOption($con).'$form .= '<br /><br />';
+		$form .= $defaults[3];
+		$form .= '</textarea>';
+		$form .= '<br /><br />';
 		
 		$form .= '<input type="submit" class="button" value="Submit Changes" />';
 		
 		$form .= '</form>';
 		$form .= '</div>';
 		
-		re3urn $form;
+		return $form;
 	}
 	
-		
-		/**
-		 * Retrieves news data from the databse and prepares it for display within the
-		 * Manage News page.
-		 * @author Tylor Faoro
-		 *
-		 * @param object $con The database object
-		 * @param str/**
+	/**
 	 * FUNCTION YET TO BE STARTED
 	*/
 	function modifyNews($con, $nid){
@@ -287,17 +272,10 @@ ID."' ";
 	*
 	* @param object $con The database object
 	* @param string $column The desired column name from the DB (same column labels from DB)
-	/
-		function getNews($con, $column, $p){
-	 	static $results = "";
-		
-		$sql = "SELECT * ";
-		$sql*
-	M news ";
-		$sql .= "WHERE NEW_ID = ".$p;
-		
-		$column = mysqli_real_escape_string($con, $column);
-		$que*/
+	* @param int    $p The integer ID number to ensure proper news article opened for modification
+	*
+	* @return mixed $results Results from the query to be used to modify current information within the DB
+	*/
 	function getNews($con, $column, $p){
 	static $results;
 
@@ -309,52 +287,41 @@ ID."' ";
 		$sql .= "JOIN region ";
 		$sql .= "ON News_Region_Assc.Region_REG_ID = region.REG_ID ";
 		$sql .= 'WHERE NEW_ID = '.$p;
-		 = $data[$column];	
-		}
-		return $results;
-	}
-	
-	/**
-	 * Retrieves the Region name and Region ID fr		
+				
+		$column = mysqli_real_escape_string($con, $column);
+		$query = mysqli_query($con, $sql);
+				
 		while($data = mysqli_fetch_array($query)){
 			$results = $data[$column];
 		}		
 		return $results;		
 	}
-	or Faoro
+		
+	/**
+	 * Retrieves the Region name and Region ID from the database, places the Name value as an option within a Form Select
+	 * element. Assigns the Region ID as the return value of the form select option element.
+	 * @author Tylor Faoro
 	 *
 	 * @param Object $con The database connection object
 	 * @return static mixed $data The options which can be selected from the Region select form element.
 	*/
-	function getRegionName($con){
+	function selectElementRegionOption($con){
 		static $data;
 		 
 		$sql  = "SELECT * ";
 		$sql .= "FROM region ";
-		
-		
+						
 		$query = mysqli_query($con, $sql);
 		
 		$results = array();
-
 			while($row = mysqli_fetch_array($query)){
 				$results[] = $row;
-				if($row['selectElementRegionOption9){
+				if($row['REG_ID'] != 99){
 					$data .= '<option value='.$row['REG_ID'].'>'.$row['REG_Name'].'</option>';
+				}
 				
-		$query = mysqli_query($con, $sql);
-		
-		$results = array();n, $id){
-		
-		
-		$sql  = 'SELECT REG_Name ';
-		$sql .= 'FROM region ';
-		$sql .= 'WHERE REG_ID = '.$id.' ';
-		
-		$query = mysqli_query($con, $sql);
-		
-		while($data = mysqli_fetch_array($query)){
-			echo $data['REG_Name	
+			}
+			return $data;	
 	}
 	
 	/**
@@ -371,39 +338,31 @@ ID."' ";
 		
 		$sql = 'SELECT * ';
 		$sql .= 'FROM news';
+		
+		$query = mysqli_query($con, $sql);
+		
+		echo '<table class="tableCenter">';
+		echo '<tr>';
+		echo '<th>News Title</th>';
+		echo '<th>News Date</th>';
+		echo '<th>News Category</th>';
+		echo '<th>News Region</th>';
+						
+		while($data = mysqli_fetch_array($query)){
+			$newsID = $data['NEW_ID']; // used to get region name using Junction Table
+			
+			echo '<tr>';
 			echo '<td>'.$data['NEW_Title'].'</td>';
 			echo '<td>'.$data['NEW_Date'].'</td>';
 			echo '<td>'.$data['NEW_Type'].'</td>';
 			echo '<td>';
-			echo getRegionNameByID($con, 101);
+					 getRegionName($con, $newsID); // Calls getRegionName() function to determine name of Region for news					 
 			echo '</td>';
-			echo '<td class="hiddenEffects				
-		while($data = mysqli_fetch_array($query)){
-			$newsID = $data['NEW_ID']; // used to get region name using Junction Table
-			odify&id='.$data['NEW_ID'].'">'.IMG("edit.gif", "Edit the Entry").'</a>';
+			echo '<td class="hiddenEffects">';
+			echo '<a href="manageNews.php?action=modify&id='.$data['NEW_ID'].'">'.IMG("edit.gif", "Edit the Entry").'</a>';
 			echo '</td>';
 			echo '</tr>';
-		}
-		
-		echo '</table>';
-			
-	}
-
-	
-	/**
-	 * Simply returns t		 getRegionName($con, $newsID); // Calls getRegionName() function to determine name of Region for news					 @return string $date A (Y/M/D H:M:S) datestring
-	*/
-	function currTimeDate(){
-		
-		$date = date('Y-m-d H:i:s');
-		
-		return $date;
-		
-	}
-	
-	
-
-?>		
+		}		
 		echo '</table>';					
 	}
 	
@@ -430,7 +389,14 @@ ID."' ";
 		while($row = mysqli_fetch_array($subQuery)){
 			echo $row['REG_Name'];
 		}						
-	}		
+	}
+	
+	/**
+	 * Simply returns the current date and time
+	 * 
+	 * @return string $date A (Y/M/D H:M:S) datestring
+	*/
+	function currTimeDate(){		
 		$date = date('Y-m-d H:i:s');
 		return $date;		
 	}	
